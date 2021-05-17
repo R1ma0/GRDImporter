d66efd4c87b727a14f6096ffa139510ba2613c11
cactusan <cactus-2015@mail.ru>
2021-05-17 10:46:45 +0300

    Implemented combining vertexes into polygons

--- ops.py
+++ ops.py
@@ -9,6 +9,8 @@ import numpy as np
 
 
 
+# OPERATORS
+
 class ImportGRDFile(Operator, ImportHelper):
     """ Opens the BlenderFileView """
     # Name for executing the operator
@@ -26,7 +28,10 @@ class ImportGRDFile(Operator, ImportHelper):
     
     def execute(self, context):
         data = self.readFile(self.filepath)
-        self.createMesh(data, "My_Object")
+        with open("D:\\VolSU\\3 курс\\2 семестр\\НИР\\GRDImporter\\data.txt", "w") as file:
+            for item in data:
+                file.write(str(data.get(item)) + '\n')
+        self.createMesh(data, "GRD_DATA_OBJ")
 
         return {'FINISHED'}
 
@@ -39,7 +44,8 @@ class ImportGRDFile(Operator, ImportHelper):
                 # Чтение и распоковка заголовков 
                 # Идентификатор файла
                 fId = file.read(4)
-                data['fId'] = fId.decode('utf-8')
+                fId = fId.decode('utf-8')
+                data['fId'] = fId
 
                 # Число колонок сетки
                 sNx = file.read(2)
@@ -53,48 +59,54 @@ class ImportGRDFile(Operator, ImportHelper):
 
                 # Минимальное значение Х на сетке 
                 xMin = file.read(8)
-                data['xMin'] = unpack('d', xMin)[0]
+                xMin = unpack('d', xMin)[0]
+                data['xMin'] = xMin
 
                 # Максимальное значение Х на сетке
                 xMax = file.read(8)
-                data['xMax'] = unpack('d', xMax)[0]
+                xMax = unpack('d', xMax)[0]
+                data['xMax'] = xMax
 
                 # Минимальное значение У на сетке 
                 yMin = file.read(8)
-                data['yMin'] = unpack('d', yMin)[0]
+                yMin = unpack('d', yMin)[0]
+                data['yMin'] = yMin
 
                 # Максимальное значение У на сетке
                 yMax = file.read(8)
-                data['yMax'] = unpack('d', yMax)[0]
+                yMax = unpack('d', yMax)[0]
+                data['yMax'] = yMax
 
                 # Минимальное значение Z на сетке 
                 zMin = file.read(8)
-                data['zMin'] = unpack('d', zMin)[0]
+                zMin = unpack('d', zMin)[0]
+                data['zMin'] = zMin
 
                 # Максимальное значение Z на сетке
                 zMax = file.read(8)
-                data['zMax'] = unpack('d', zMax)[0]
+                zMax = unpack('d', zMax)[0]
+                data['zMax'] = zMax
                 
                 # Общее число элементов сетки
                 N = sNx * sNy
-                data['N'] = N
 
                 # Чтение основных данных
                 Z = [ [0] * 3 for i in range(N) ]
 
-                xCoord = 0
+                xCoord = 1
                 yCoord = 0
 
                 for i in range(N):
                     zCoord = file.read(4)
-                    zCoord = unpack("f", zCoord)[0]
+                    zCoord = unpack('f', zCoord)[0]
+                    zCoord = round(zCoord, 3)
 
                     Z[i] = [xCoord, yCoord, zCoord]
-
+                    
                     if xCoord < sNx:
                         xCoord += 1
                     else:
-                        xCoord = 0
+                        xCoord = 1
                         yCoord += 1
 
                 data['Z'] = Z
@@ -105,7 +117,7 @@ class ImportGRDFile(Operator, ImportHelper):
         # Mesh data
         vertices = data['Z']
         edges    = []
-        faces    = []
+        faces    = self.mergeVertexesIntoPolygons(data)
 
         # Create mesh
         mesh = bpy.data.meshes.new(objhName)
@@ -121,11 +133,30 @@ class ImportGRDFile(Operator, ImportHelper):
         meshObj.location.x = 0.0
         meshObj.location.y = 0.0
         meshObj.location.z = 0.0
+        meshObj.scale = [0.1, 0.1, 0.1]
 
         # Add object to scene
         bpy.context.scene.collection.objects.link(meshObj)
 
         return meshObj
+
+    def mergeVertexesIntoPolygons(self, data):
+        faces = []
+        rowMaxLength = data['sNx']
+
+        for row in range(1, data['sNy'] - 1, 1): # TODO: Должно быть без -1. Последняя строка крашит
+            for column in range(data['sNx'] - 1):
+                previousRow = row -1 
+                face = (
+                    column + previousRow * rowMaxLength, 
+                    column + previousRow * rowMaxLength + 1, 
+                    column + row * rowMaxLength + 1, 
+                    column + row * rowMaxLength
+                )
+                faces.append(face)
+
+        return faces
+
 
 
 def register():
