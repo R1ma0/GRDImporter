2d8863e1cd7a833d16323b0312685ed814cec2da
cactusan <cactus-2015@mail.ru>
2021-06-05 20:29:36 +0300

    Changed UI placement. Code refactored

--- io_import_grd.py
+++ io_import_grd.py
@@ -4,8 +4,20 @@ import bpy
 from bpy_extras.io_utils import ImportHelper
 from bpy.props import StringProperty
 from bpy.types import Operator
-# Modul for working with binary data
 from struct import unpack # Method for unpacking data
+
+### ADD-ON INFORMATION
+
+bl_info = {
+    "name": ".GRD Importer",
+    "author": "Abdrazackov Damir",
+    "description": "Import Format Files .GRD",
+    "version": (1, 3, 0),
+    "blender": (2, 91, 2),
+    "location": "File > Import > Surfer 6 Binary (.grd)", 
+    "category": "Import-Export",
+    "support": "COMMUNITY",
+}
 
 ### OPERATORS
 
@@ -13,7 +25,7 @@ class ImportGRDFile(Operator, ImportHelper):
     """ Opens The BlenderFileView """
     # Name for executing the operator
     bl_idname = "import_file.import_grd_file"
-    bl_label = "Import"
+    bl_label = "Surfer 6 Binary (.grd)"
     
     # File format for import
     filename_ext = ".grd"
@@ -25,11 +37,11 @@ class ImportGRDFile(Operator, ImportHelper):
     )
     
     def execute(self, context):
-        data = self.readFile(self.filepath)
-        self.createMesh(data, "GRD_DATA_OBJ")
+        data = self.read_file(self.filepath)
+        self.create_mesh(data, "GRD_DATA_OBJ")
         return {'FINISHED'}
 
-    def readFile(self, fileName):
+    def read_file(self, fileName):
         """ Reading A Binary File """
         # Dictionary for storing read values
         data = dict()
@@ -40,35 +52,25 @@ class ImportGRDFile(Operator, ImportHelper):
             fId = file.read(4)
             fId = fId.decode('utf-8')
             data['fId'] = fId
-            # Number of columns
-            sNx = self.readAndUnpuck(file, 2)
-            data['sNx'] = sNx
-            # Number of rows
-            sNy = self.readAndUnpuck(file, 2)
-            data['sNy'] = sNy
-            # Minimum value of X on the grid
-            data['xMin'] = self.readAndUnpuck(file, 8)
-            # Maximum value if X on the grid
-            data['xMax'] = self.readAndUnpuck(file, 8)
-            # Minimum value of Y on the grid
-            data['yMin'] = self.readAndUnpuck(file, 8)
-            # Maximum value if Y on the grid
-            data['yMax'] = self.readAndUnpuck(file, 8)
-            # Minimum value of Y on the grid 
-            data['zMin'] = self.readAndUnpuck(file, 8)
-            # Maximum value if Z on the grid
-            data['zMax'] = self.readAndUnpuck(file, 8)
+            # Number of columns and rows
+            rowsColumnsHeaderProperties = ['sNx', 'sNy']
+            for property in rowsColumnsHeaderProperties:
+                data[property] = self.read_and_unpuck(file, 2)
+            # Minimum and maximum values of X, Y, Z coordinates
+            xyzHeaderProperties = ['xMin', 'xMax', 'yMin', 'yMax', 'zMin', 'zMax']
+            for property in xyzHeaderProperties:
+                data[property] = self.read_and_unpuck(file, 8)
             # Total number of grid elements
-            N = sNx * sNy
+            N = data['sNx'] * data['sNy']
             # Reading data
             Z = [ [0] * 3 for i in range(N) ]
             xCoord = 1
             yCoord = 0
             for i in range(N):
-                zCoord = self.readAndUnpuck(file, 4)
+                zCoord = self.read_and_unpuck(file, 4)
                 zCoord = round(zCoord, 6)
                 Z[i] = [xCoord, yCoord, zCoord]
-                if xCoord < sNx:
+                if xCoord < data['sNx']:
                     xCoord += 1
                 else:
                     xCoord = 1
@@ -76,24 +78,22 @@ class ImportGRDFile(Operator, ImportHelper):
             data['Z'] = Z
         return data
 
-    def readAndUnpuck(self, file, size):
+    def read_and_unpuck(self, file, size):
         """ Reading And Unpacking Binary Data """
         element = file.read(size)
-        if size == 8:
+        if size == 8: # Double
             element = unpack('d', element)[0]
-            return element
-        elif size == 2:
+        elif size == 2: # Short
             element = unpack('h', element)[0]
-            return element
-        elif size == 4:
+        elif size == 4: # Float
             element = unpack('f', element)[0]
-            return element
+        return element
     
-    def createMesh(self, data, objhName):
+    def create_mesh(self, data, objhName):
         """ Creating A Grid Based On Data """
         # Mesh data
         vertices = data['Z']
-        faces    = self.mergeVertexesIntoPolygons(data)
+        faces    = self.merge_vertexes_into_polygons(data)
         # Create mesh
         mesh = bpy.data.meshes.new(objhName)
         # Generate mesh data
@@ -111,28 +111,40 @@ class ImportGRDFile(Operator, ImportHelper):
         bpy.context.scene.collection.objects.link(meshObj)
         return meshObj
 
-    def mergeVertexesIntoPolygons(self, data):
+    def merge_vertexes_into_polygons(self, data):
         """ Connecting Vertexes To Polygons """
         faces = []
-        rowMaxLength = data['sNx']
         for row in range(1, data['sNy'], 1):
-            for column in range(rowMaxLength - 1):
+            for column in range(data['sNx'] - 1):
                 previousRow = row -1 
                 face = (
-                    column + previousRow * rowMaxLength, 
-                    column + previousRow * rowMaxLength + 1, 
-                    column + row * rowMaxLength + 1, 
-                    column + row * rowMaxLength
+                    column + previousRow * data['sNx'], 
+                    column + previousRow * data['sNx'] + 1, 
+                    column + row * data['sNx'] + 1, 
+                    column + row * data['sNx']
                 )
                 faces.append(face)
         return faces
+
+### UI
+
+def set_ui(self, context):
+    """ Implements The Addon Interface """
+    self.layout.operator(ImportGRDFile.bl_idname, text="Surfer 6 Binary (.grd)")
 
 ### REG- & UNREGISTRATION
 
 def register():
     """ Registering Add-on Classes """
     bpy.utils.register_class(ImportGRDFile)
+    bpy.types.TOPBAR_MT_file_import.append(set_ui)
     
 def unregister():
     """ Unregistering Add-on Classes """
     bpy.utils.unregister_class(ImportGRDFile)
+    bpy.types.TOPBAR_MT_file_import.remove(set_ui)
+
+### ADD-ON EXIT POINT
+
+if __name__ == "__main__":
+    register()
